4db839f4998907ff24d8050d9bab009485b9e91f
yasinadil <adilyasin205@gmail.com>
2023-03-06 06:40:21 +0500

    new contracts

--- utils/Config.ts
+++ utils/Config.ts
@@ -1,3 +1,3 @@
 export const accessContractAddress =
   "0xD76009e6AAB24166a261431800BFf8DdDAaaa56f";
-export const subscriptionAddress = "0x206d5eb817533e6228f0340094FB9900bC018b9b";
+export const subscriptionAddress = "0xD2cAbFbE1b71496C53F798c1ba30233ed35e8B1C";
